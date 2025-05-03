432728271ca275f73a8544178e906e2bd6cfbb45
mickael decouflet <contact@mickaeldecouflet.fr>
2025-05-03 18:23:52 +0200

    fix(take) final picture

--- src/routes/take/index.tsx
+++ src/routes/take/index.tsx
@@ -20,26 +20,26 @@ export default component$(() => {
   useStyles$(styles);
   const nav = useNavigate();
 
-  const photo = useSignal<string[]>([]);
+  const hideButton = useSignal(false);
 
   const time = useSignal(-1);
 
   const currentPhoto = useSignal<string | null>();
 
-  const maxPhoto = 4;
-
   const trigger = $(() => {
     currentPhoto.value = null;
     time.value = 5;
+    hideButton.value = true;
 
     const interval = setInterval(async () => {
       time.value -= 1;
+
       if (time.value < 0) {
         clearInterval(interval);
         const time = new Date();
         await takePitcture(time.toISOString())
-	console.log('okay finish');
         currentPhoto.value = `/photos/${time.toISOString()}.jpg`;
+        hideButton.value = false;
       }
     }, 1e3)
   })
@@ -50,27 +50,6 @@ export default component$(() => {
 
     console.log('redirect end');
   })
-    
-
-  const nextPhoto = $(() => {
-    photo.value = [...photo.value, currentPhoto.value as string];
-
-    trigger()
-  });
-
-  if (photo.value.length === 4) {
-    return (
-      <section class="take">
-        <p>Vos Photos</p>
-        <div class="photos">
-            {photo.value.map((p) => (
-              <img src={p} alt={p} key={p} height={108} width={192} />
-            ))}
-        </div>
-        <button type="button" onClick$={() => print()}>Print</button>
-      </section>
-    )
-  }
 
   return (
     <section class="take">
@@ -78,27 +57,13 @@ export default component$(() => {
         {currentPhoto.value && (<img src={currentPhoto.value} alt="last take" height={432} width={768}/>)}
         <div class="content">
         {time.value !== -1 && <p class="conteur">{time.value}</p>}
-        {
-          !currentPhoto.value ? time.value === -1 && (
-            <button type="button" onClick$={() => trigger()}>Demarrer</button>
-          ) : (
-            <>
-              <button type="button" onClick$={() => trigger()} class="space">Reprendre</button>
-              <button type="button" onClick$={() => nextPhoto()}>Prendre la photo suivante</button>
-            </>
-          )
-        }
-        </div>
-      </div>
-      <div>
-        <h2>Voici votre photo ({photo.value.length}/4)</h2>
+        {!hideButton.value  && !currentPhoto.value && (<button type="button" onClick$={() => trigger()}>Demarrer</button>)}
 
-        <div class="photos">
-          {photo.value.map((p) => (
-            <img class="preview-image" src={p} alt={p} key={p} height={108} width={192} />
-          ))}
-
-          {new Array(maxPhoto - photo.value.length).fill(0).map((_, i) => (<div class="empty" key={`p${i}`}>Photo {photo.value.length + i + 1}</div>))}
+        {!hideButton.value && currentPhoto.value &&  (<>
+          <button type="button" onClick$={() => trigger()}>Prendre une nouvelle photo</button>
+          <a href="/">Terminé</a>
+          </>
+          )}
         </div>
       </div>
     </section>);
